354165e3531d557713e7b1008998b7ff4b3cdeb8
lmathieu <63464538+lmathieu@users.noreply.github.com>
2020-04-10 17:54:06 +0200

    Add files via upload

--- ListenerAccueil.java
+++ ListenerAccueil.java
@@ -5,14 +5,34 @@ import java.awt.*;
 
 public class ListenerAccueil implements ActionListener{
 	private fenetreMorpion fen;
-	//private MorpionInterface inter;
-	
-	public ListenerAccueil(fenetreMorpion fen){
+	private String nomjoueur1;
+	private String nomjoueur2;
+
+
+public ListenerAccueil(fenetreMorpion fen){
 		this.fen = fen;
+		
+
 	}
 	
 	public void actionPerformed(ActionEvent e){
-		new MorpionInterface();
-	}
 	
+		int x=1;
+		int y=2;
+		
+		String nomjoueur1=fen.getNomJoueur(x);
+		String nomjoueur2=fen.getNomJoueur(y);
+	
+	
+		if(nomjoueur1==null || nomjoueur1.equals("")|| nomjoueur2==null || nomjoueur2.equals("")){ //on met condition "" car au départ la case n'est jamais null
+			
+			JOptionPane.showMessageDialog(null,"Inserez le nom des joueurs!");
+		}else{
+			new MorpionInterface();
+		
+		}
+	}
 }
+	
+	
+
