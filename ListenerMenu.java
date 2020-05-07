091a62fd08f4e3301f99a4c912e6714ef59659c9
lmathieu <63464538+lmathieu@users.noreply.github.com>
2020-05-07 23:33:29 +0200

    Update ListenerMenu.java

--- ListenerMenu.java
+++ ListenerMenu.java
@@ -5,7 +5,7 @@ import java.awt.*;
 
 public class ListenerMenu implements ActionListener{
 	private MorpionInterface inter;
-	private fenetreMorpion fen;
+	private fenetreAccueil fen;
 
 	public ListenerMenu(MorpionInterface inter){
 		this.inter = inter;
